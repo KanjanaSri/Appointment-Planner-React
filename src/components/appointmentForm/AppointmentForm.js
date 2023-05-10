5271512731d14ba78d4038258a4815820dfd3420
SKarnjana <ksrisumrarn.pam@gmail.com>
2023-05-10 15:03:10 -0700

    implement appointments

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useMemo } from "react";
+
+import { ContactPicker } from "../contactPicker/ContactPicker";
 
 const getTodayString = () => {
   const [month, day, year] = new Date()
@@ -19,8 +21,63 @@ export const AppointmentForm = ({
   setTime,
   handleSubmit
 }) => {
+  const contactNames = useMemo(() => {
+    return contacts.map((contact) => contact.name);
+  }, [contacts]);
 
   return (
-    <></>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input
+            type="text"
+            name="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+            placeholder="Appointment Name"
+            aria-label="Appointment Name"
+          />
+        </label>
+        <br />
+
+        <label>
+          <ContactPicker
+            name="contact"
+            value={contact}
+            contacts={contactNames}
+            onChange={(e) => setContact(e.target.value)}
+          />
+        </label>
+        <br />
+
+        <label>
+          <input
+            type="date"
+            name="date"
+            min={getTodayString()}
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+            required
+            aria-label="Date Picker"
+          />
+        </label>
+        <br />
+
+        <label>
+          <input
+            type="time"
+            name="time"
+            value={time}
+            onChange={(e) => setTime(e.target.value)}
+            required
+            aria-label="Time Picker"
+          />
+        </label>
+        <br />
+
+        <input aria-label="Add Appointment" type="submit" value="Add Appointment" />
+      </form>
+    </>
   );
 };
